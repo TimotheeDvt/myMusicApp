acc572054cf26d37a1da85503ae7e34b7e4a07d2
TimotheeDvt <timothee.dravet@gmail.com>
2026-04-20 09:37:40 +0200

    feat: refactor voicing data structure in ShellVoicing component for improved formula handling

--- js/components/shell-voicing.js
+++ js/components/shell-voicing.js
@@ -3,50 +3,46 @@ class ShellVoicing extends HTMLElement {
         super();
         this.attachShadow({ mode: 'open' });
 
+        // Base fret X-positions for your 4-fret span
         const Xs = [3.35, 6.35, 9.35];
 
-        this.voicingData = {
-            "maj7": {
-                "E": [[5, Xs[1], "R"], [4, Xs[0], "3"], [3, Xs[2], "7"]],
-                "A": [[4, Xs[1], "R"], [3, Xs[0], "3"], [2, Xs[2], "7"]],
-                "D": [[3, Xs[1], "R"], [2, Xs[0], "3"], [1, Xs[2], "7"]],
-                "G": [[2, Xs[0], "R"], [1, Xs[0], "3"], [0, Xs[2], "7"]]
-            },
-            "min7": {
-                "E": [[5, Xs[2], "R"], [4, Xs[0], "b3"], [3, Xs[2], "b7"]],
-                "A": [[4, Xs[2], "R"], [3, Xs[0], "b3"], [2, Xs[2], "b7"]],
-                "D": [[3, Xs[1], "R"], [2, 0.75, "b3"], [1, Xs[2], "b7"]],
-                "G": [[2, Xs[1], "R"], [1, Xs[0], "b3"], [0, Xs[2], "b7"]]
-            },
-            "dom7": {
-                "E": [[5, Xs[1], "R"], [4, Xs[0], "3"], [3, Xs[1], "b7"]],
-                "A": [[4, Xs[1], "R"], [3, Xs[0], "3"], [2, Xs[1], "b7"]],
-                "D": [[3, Xs[1], "R"], [2, Xs[0], "3"], [1, Xs[2], "b7"]],
-                "G": [[2, Xs[1], "R"], [1, Xs[1], "3"], [0, Xs[2], "b7"]]
-            },
-            "6": {
-                "E": [[5, Xs[1], "R"], [4, Xs[0], "3"], [3, Xs[0], "6"]],
-                "A": [[4, Xs[1], "R"], [3, Xs[0], "3"], [2, Xs[0], "6"]],
-                "D": [[3, Xs[1], "R"], [2, Xs[0], "3"], [1, Xs[1], "6"]],
-                "G": [[2, Xs[1], "R"], [1, Xs[1], "3"], [0, Xs[1], "6"]]
-            }
+        // FORMULAS: [ [relString, relFretIdx, label], ... ]
+        // relString: 0 is the root string, -1 is one string higher (closer to floor)
+        // relFretIdx: index into the Xs array (0, 1, or 2)
+        this.formulas = {
+            "maj7": [[0, 1, "R"], [-1, 0, "3"], [-2, 2, "7"]],
+            "min7": [[0, 2, "R"], [-1, 0, "b3"], [-2, 2, "b7"]],
+            "dom7": [[0, 1, "R"], [-1, 0, "3"], [-2, 1, "b7"]],
+            "6": [[0, 1, "R"], [-1, 0, "3"], [-2, 0, "6"]]
         };
+
+        // Map root string names to their index in the ['E','B','G','D','A','E'] array
+        this.stringMap = { "E": 5, "A": 4, "D": 3, "G": 2 };
     }
 
-    connectedCallback() {
-        this.render();
-    }
+    connectedCallback() { this.render(); }
 
     render() {
         const type = this.getAttribute('type') || 'maj7';
-        const rootStr = this.getAttribute('root-string') || 'E';
-        const data = this.voicingData[type][rootStr] || [];
-        const strings = ['E', 'B', 'G', 'D', 'A', 'E'];
+        const rootStrName = this.getAttribute('root-string') || 'E';
 
+        const formula = this.formulas[type];
+        const rootIdx = this.stringMap[rootStrName];
+        const Xs = [3.35, 6.35, 9.35];
+
+        const activeMarkers = formula.map(([sOffset, fIdx, label]) => {
+            return {
+                stringIdx: rootIdx + sOffset,
+                xPos: Xs[fIdx],
+                label: label
+            };
+        });
+
+        const strings = ['E', 'B', 'G', 'D', 'A', 'E'];
         let stringsHtml = strings.map((label, idx) => {
-            const markerData = data.find(m => m[0] === idx);
-            const markerHtml = markerData
-                ? `<div class="note-marker" style="left: calc(100% / 12 * ${markerData[1]});">${markerData[2]}</div>`
+            const marker = activeMarkers.find(m => m.stringIdx === idx);
+            const markerHtml = marker
+                ? `<div class="note-marker" style="left: calc(100% / 12 * ${marker.xPos});">${marker.label}</div>`
                 : '';
 
             return `
